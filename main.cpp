2ec35cdbeede03d42c61107c82d58bcbbc573796
HuntingSHEEP <a.glowacki98@gmail.com>
2021-05-11 23:45:35 +0200

    silnik fizyki: grawitacja i sterowanie przyspieszeniem

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 #include <gtk/gtk.h>
 #include <iostream>
 #include <thread>
-#include<unistd.h>
+#include <unistd.h>
+#include <math.h>
 
 
 using namespace std;
@@ -28,6 +29,11 @@ class Point{
         void setPosition(double x, double y){
             this->x = x;
             this->y = y;
+        }
+
+        void moveBy(double dx, double dy){
+            this->x += dx;
+            this->y += dy;
         }
 
     private:
@@ -115,6 +121,170 @@ class Crosshair{
         }
 };
 
+
+class Vector{
+public:
+    double x, y, z;
+
+    Vector(double x, double y){
+        this->x = x;
+        this->y = y;
+        this->z = 0;
+    }
+
+    Vector(double x, double y, double z){
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+
+    void add(Vector vector){
+        this->x += vector.x;
+        this->y += vector.y;
+        this->z += vector.z;
+    }
+
+    void add(double x, double y, double z){
+        this->x += x;
+        this->y += y;
+        this->z += z;
+    }
+
+    void multiply(double scale){
+        this->x *= scale;
+        this->y *= scale;
+        this->z *= scale;
+    }
+
+    double length(){
+        return sqrt(x*x + y*y + z*z);
+    }
+};
+
+class Object{
+public:
+    int width, height;
+    Point leftUpperVertex;
+
+    void setPosition(Point p){
+        this->leftUpperVertex.setPosition(p.x, p.y);
+    }
+
+    void setPosition(double x, double y){
+        this->leftUpperVertex.setPosition(x, y);
+    }
+
+
+    void moveBy(double dx, double dy){
+        this->leftUpperVertex.moveBy(dx, dy);
+    }
+
+    Point getPosition(){
+        return leftUpperVertex;
+    }
+
+};
+
+class PhysicComponent: public Object{
+public:
+    Vector v, a;
+    PhysicComponent(Vector v, Vector a) : v(v), a(a){}
+
+    void addForce(Vector vector){
+        v.add(vector);
+    }
+
+    void addAcceleration(Vector vector){
+        a.add(vector);
+    }
+
+    void setGravity(double g){
+        addAcceleration(Vector(0, g));
+    }
+
+    void calculatePhysic(double deltaTime){
+        //zmiana położenia
+        double dx = v.x*deltaTime + (a.x/2)*deltaTime*deltaTime;
+        double dy = v.y*deltaTime + (a.y/2)*deltaTime*deltaTime;
+        //nowa prędkość
+        double dvx = a.x*deltaTime;
+        double dvy = a.y*deltaTime;
+        //aktualizacja położenia
+        leftUpperVertex.moveBy(dx, dy);
+        //aktualizacja prędkości
+        v.add(dvx, dvy, 0);
+    }
+};
+
+class Rectangle: public PhysicComponent{
+public:
+
+    Rectangle(int width, int height)
+        : PhysicComponent(Vector(0, 0, 0), Vector(0, 0))
+    {
+
+        this->width = width;
+        this->height = height;
+        this->leftUpperVertex = Point(0, 0);
+
+    }
+
+    Rectangle(int width, int height, Point leftUpperVertex)
+        : PhysicComponent(Vector(0, 0, 0), Vector(0, 0))
+    {
+        this->width = width;
+        this->height = height;
+        this->leftUpperVertex = leftUpperVertex;
+    }
+
+
+    void drawMe(cairo_t *cr, GdkRGBA *color){
+        cairo_rectangle(cr, leftUpperVertex.x, leftUpperVertex.y, width, height);
+        gdk_cairo_set_source_rgba (cr, color);
+        cairo_fill(cr);
+    }
+
+
+};
+
+class Key{
+public:
+    bool isPressed;
+    bool pressedAgain;
+    bool resetDone;
+
+    Key(){
+        this->isPressed = false;
+        this->pressedAgain = false;
+        this->resetDone = true;
+    }
+
+    Key(bool isPressed, bool pressedAgain){
+        this->isPressed = isPressed;
+        this->pressedAgain = pressedAgain;
+        this->resetDone = true;
+    }
+};
+
+
+class Input{
+public:
+    Key wPressed;
+    Key sPressed;
+    Key dPressed;
+    Key aPressed;
+
+    Input(){
+        wPressed = Key();
+        sPressed = Key();
+        dPressed = Key();
+        aPressed = Key();
+    }
+};
+
+
+//*****************************************************************************************************************
+
 int n = 1;
 int z = 1;
 
@@ -133,6 +303,11 @@ double yk = 50;
 
 Line kreska = Line(Point(40, 40), Point(400, 160));
 Crosshair celownik = Crosshair(Point(50, 50), 40, 5);
+
+Rectangle kwadrat = Rectangle(40, 40, Point(400, 200));
+Rectangle kwadrat2 = Rectangle(40, 40, Point(500, 200));
+
+Input klawiatura = Input();
 
 void wyswietlaniePozycjiOkna(GtkWindow *okno, GdkEvent *zdarzenie, gpointer dane){
     int x, y;
@@ -154,16 +329,24 @@ gboolean draw_callback (GtkWidget *widget, cairo_t *cr, gpointer data)
     height = gtk_widget_get_allocated_height (widget);
 
     //cairo_arc (cr, n, height / 2.0,MIN (width, height) / 2.0,0, 2 * G_PI);
-
+    //białe tło
     cairo_rectangle(cr, 0, 0, width, height);
     gdk_cairo_set_source_rgba (cr, &color);
     cairo_fill (cr);
 
+
     color = { 0,0,1,1};
-    cairo_rectangle(cr, xk, yk, 50, 50);
+    //cairo_rectangle(cr, xk, yk, 50, 50);
     gdk_cairo_set_source_rgba (cr, &color);
+
     cairo_fill (cr);
 
+
+    kwadrat.drawMe(cr, &color);
+    color = { 1,0,0,1};
+    kwadrat2.drawMe(cr, &color);
+
+    color = { 0,1,1,1};
 
     //draw to me the LINE
     celownik.drawMe(cr, &color);
@@ -179,41 +362,82 @@ void buttonFunction (GtkButton *button, gpointer user_data) /* No extra paramete
     int *pn = (gint *) user_data;
     *pn += 10;
     //cout<<*pn<<endl;
+    cout<<"BEE:"<<endl;
+    kwadrat2.addAcceleration(Vector(0, -15, 0));
 }
 
-bool wPressed = false;
-bool dPressed = false;
-bool sPressed = false;
-bool aPressed = false;
+
 
 [[noreturn]] void silnikFizyki(){
     double microsecond = 5*1000;
-    double deltaTime = 1000000/microsecond;
-
-    int skok = 1;
+    double deltaTime = microsecond/150000;
+    int skok = 10;
 
     while(true){
 
-        if(wPressed){
-            yk -= skok;
-        }else if(sPressed){
-            yk += skok;
+        //OŚ PIONOWA
+
+        if(klawiatura.wPressed.isPressed & (!klawiatura.wPressed.pressedAgain) ){
+
+            kwadrat2.addAcceleration(Vector(0, -skok));
+            klawiatura.wPressed.resetDone = false;
+            klawiatura.wPressed.pressedAgain = true;
+            cout<<"do góry"<<endl;
+        }else if(klawiatura.sPressed.isPressed & (! klawiatura.sPressed.pressedAgain)){
+
+            kwadrat2.addAcceleration(Vector(0, skok));
+            klawiatura.sPressed.resetDone = false;
+            klawiatura.sPressed.pressedAgain = true;
+            cout<<"w dół"<<endl;
         }
 
-        if(dPressed){
-            xk+=skok;
-        }else if(aPressed){
-            xk-= skok;
+        //OŚ POZIOMA
+
+        if(klawiatura.aPressed.isPressed & (!klawiatura.aPressed.pressedAgain) ){
+
+            kwadrat2.addAcceleration(Vector(-skok, 0));
+            klawiatura.aPressed.resetDone = false;
+            klawiatura.aPressed.pressedAgain = true;
+            cout<<"w lewo"<<endl;
+        }else if(klawiatura.dPressed.isPressed & (! klawiatura.dPressed.pressedAgain)){
+
+            kwadrat2.addAcceleration(Vector( skok, 0));
+            klawiatura.dPressed.resetDone = false;
+            klawiatura.dPressed.pressedAgain = true;
+            cout<<"w prawo"<<endl;
         }
 
 
-        t += microsecond/1000000;
-        yk = yk + v0*t + (a/2)*t*t;
-        cout<<yk<<endl;
+
+
+        //blok rezygnacji
+
+        if((!klawiatura.wPressed.isPressed) & (!klawiatura.wPressed.resetDone)){
+            kwadrat2.addAcceleration(Vector( 0, skok ));
+            klawiatura.wPressed.resetDone = true;
+            cout<<"reset do góry"<<endl;
+        }else if((!klawiatura.sPressed.isPressed) & (!klawiatura.sPressed.resetDone)){
+            kwadrat2.addAcceleration(Vector(0, -skok));
+            klawiatura.sPressed.resetDone = true;
+            cout<<"reset w dół"<<endl;
+        }
+
+
+        if((!klawiatura.aPressed.isPressed) & (!klawiatura.aPressed.resetDone)){
+            kwadrat2.addAcceleration(Vector( skok, 0));
+            klawiatura.aPressed.resetDone = true;
+            cout<<"reset w lewo"<<endl;
+        }else if((!klawiatura.dPressed.isPressed) & (!klawiatura.dPressed.resetDone)){
+            kwadrat2.addAcceleration(Vector(-skok, 0));
+            klawiatura.dPressed.resetDone = true;
+            cout<<"reset w prawo"<<endl;
+        }
 
 
 
-        //cout<<"BEEEE"<<endl;
+        kwadrat2.calculatePhysic(deltaTime);
+        kwadrat.calculatePhysic(deltaTime);
+
         usleep( microsecond);//sleeps for 3 second
     }
 
@@ -221,69 +445,66 @@ bool aPressed = false;
 
 
 void wcisnietoGuzik(GtkWidget *widget, GdkEventKey *event, gpointer data){
-    //cout<<"keyval: "<<event->keyval<<"; hardware_keycode :"<<event->hardware_keycode<<"; string :"<<event->string<<";state : "<<event->state<<"; group :"<<event->group <<";  w:  "<<GDK_KEY_w<<endl;
-
     if(event->keyval == GDK_KEY_w){
-        //z-=5;
-        wPressed = true;
+        if(!klawiatura.wPressed.isPressed){
+            klawiatura.wPressed.isPressed = true;
+        }else{
+            klawiatura.wPressed.pressedAgain = true;
+        }
+
     }
 
     if(event->keyval == GDK_KEY_s){
-        //z+=5;
-        sPressed = true;
+        if(!klawiatura.sPressed.isPressed){
+            klawiatura.sPressed.isPressed = true;
+        }else{
+            klawiatura.sPressed.pressedAgain = true;
+        }
     }
 
     if(event->keyval == GDK_KEY_a) {
-        //n -= 5;
-        aPressed = true;
+        if(!klawiatura.aPressed.isPressed){
+            klawiatura.aPressed.isPressed = true;
+        }else{
+            klawiatura.aPressed.pressedAgain = true;
+        }
     }
 
     if(event->keyval == GDK_KEY_d){
-        //n+=5;
-        dPressed = true;
+        if(!klawiatura.dPressed.isPressed){
+            klawiatura.dPressed.isPressed = true;
+        }else{
+            klawiatura.dPressed.pressedAgain = true;
+        }
     }
 
     if(event->hardware_keycode == 16){
         cout<<"MOUSE CLICKED"<<endl;
     }
 
-    /*
-    if(wPressed){
-        z -= 5;
-    }else if(sPressed){
-        z += 5;
-    }
-
-    if(dPressed){
-        n+=5;
-    }else if(aPressed){
-        n -= 5;aa
-    }
-     */
-
 }
 
 void puszczonoGuzik(GtkWidget *widget, GdkEventKey *event, gpointer data){
-    cout<<"puszczono guzik"<<event->keyval<<endl;
+    //cout<<"puszczono guzik "<<event->keyval<<endl;
 
     if(event->keyval == GDK_KEY_w){
-        //z-=5;
-        wPressed = false;
+        klawiatura.wPressed.isPressed = false;
+        klawiatura.wPressed.pressedAgain = false;
     }
 
     if(event->keyval == GDK_KEY_s){
-        //z+=5;
-        sPressed = false;
+        klawiatura.sPressed.isPressed = false;
+        klawiatura.sPressed.pressedAgain = false;
     }
 
     if(event->keyval == GDK_KEY_a) {
-        //n -= 5;
-        aPressed = false;
+        klawiatura.aPressed.isPressed = false;
+        klawiatura.aPressed.pressedAgain = false;
     }
 
     if(event->keyval == GDK_KEY_d){
-        //n+=5;
-        dPressed = false;
+        klawiatura.dPressed.isPressed = false;
+        klawiatura.dPressed.pressedAgain = false;
     }
 
 }
@@ -307,6 +528,9 @@ int main (int argc, char *argv[]) {
     GtkWidget *okno;
     GtkWidget *kontener, *przycisk;
     gtk_init (&argc, &argv);
+
+    //kwadrat.setGravity(10);
+    //kwadrat2.setGravity(5);
 
     okno = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     kontener = gtk_fixed_new();
